21adfacc7d58da87c538f5b7d21d5ff7b582b600
Josina Palamoottil Joy <66690240+josinapjoy@users.noreply.github.com>
2021-11-16 16:28:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return "<h1>This is flask application </h1>"
+    return "<h1>This is Flask application </h1>"
 
 
 if __name__ == "__main__":
